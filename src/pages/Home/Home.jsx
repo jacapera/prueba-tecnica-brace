808948d995c0e547790e3e969035221083ec6caf
jacapera <capera125@gmail.com>
2023-10-05 18:33:04 -0500

    ver por consola actors

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -11,6 +11,7 @@ import { useDispatch, useSelector } from 'react-redux'
 const Home = () => {
 
   const actors = useSelector(selectActors);
+  console.log("actors: ", actors)
 
   const dispatch = useDispatch();
   useEffect(() => {
